9bd0c166dede9e1b5cd09e78432a746b15ac7756
devcomfort <kimdonghyun026@gmail.com>
2023-01-11 01:13:02 +0900

    test code (revising)

---
+++ src/Resources/index.test.ts
@@ -0,0 +1,11 @@
+import RequestHandler from '.';
+import Joi from 'joi';
+
+test('Resources TEST', async () => {
+	// 기본값 검사
+	expect(RequestHandler().getRetry()).toEqual(5);
+	expect(RequestHandler().getLimit()).toEqual(20);
+	expect(RequestHandler().getOffset()).toEqual(20);
+
+	const dump = await RequestHandler().request();
+});
