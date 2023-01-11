c8a5afc7c19bb9ffcfe40ddc13441bf830d51e16
hmyo2853 <hmyo2853@gmail.com>
2023-01-11 22:05:16 +0900

    feat : 주석처리 추가

--- src/Encounters/types.ts
+++ src/Encounters/types.ts
@@ -1,9 +1,13 @@
 import {NamedAPIResource} from '../Resources';
 
 interface EncounterMethod {
+	/** The identifier for this resource. */
 	id: number;
+	/** The name for this resource. */
 	name: string;
+	/** A good value for sorting. */
 	order: number;
+	/** The name of this resource listed in different languages. */
 	names: Array<Name>;
 }
 
