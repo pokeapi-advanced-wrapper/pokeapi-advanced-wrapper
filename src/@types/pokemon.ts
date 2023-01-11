edbf1b7a9a5ea53957df4fda7d73f03d24573794
hmyo2853 <hmyo2853@gmail.com>
2023-01-12 01:36:28 +0900

    feat: type 선언 중

--- src/@types/pokemon.ts
+++ src/@types/pokemon.ts
@@ -1,4 +1,14 @@
-import {Description, Effect, Name, NamedAPIResource, VerboseEffect, VersionGameIndex} from './common_types';
+import {
+	APIResource,
+	Description,
+	Effect,
+	FlavorText,
+	Name,
+	NamedAPIResource,
+	VerboseEffect,
+	VersionEncounterDetail,
+	VersionGameIndex,
+} from './common_types';
 
 export interface Abilities {
 	/** The identifier for this resource. */
@@ -213,3 +223,268 @@ export interface Pokemon {
 	/** A list of details showing types this Pokémon has. */
 	types: Array<PokemonType>;
 }
+
+export interface PokemonAbility {
+	/** Whether or not this is a hidden ability. */
+	is_hidden: boolean;
+	/** The slot this ability occupies in this Pokémon species. */
+	slot: number;
+	/** The ability the Pokémon may have. */
+	ability: NamedAPIResource;
+}
+
+export interface PokemonType {
+	/** The order the Pokémon's types are listed in. */
+	slot: number;
+	/** The type the referenced Pokémon has. */
+	type: NamedAPIResource;
+}
+
+export interface PokemonFormType {
+	/** The order the Pokémon's types are listed in. */
+	slot: number;
+	/** The type the referenced Form has. */
+	type: NamedAPIResource;
+}
+
+export interface PokemonTypePast {
+	/** The last generation in which the referenced pokémon had the listed types. */
+	generation: NamedAPIResource;
+	/** The types the referenced pokémon had up to and including the listed generation. */
+	types: Array<PokemonType>;
+}
+
+export interface PokemonHeldItem {
+	/** The item the referenced Pokémon holds. */
+	item: NamedAPIResource;
+	/** The details of the different versions in which the item is held. */
+	version_details: Array<PokemonHeldItemVersion>;
+}
+
+export interface PokemonHeldItemVersion {
+	/** The version in which the item is held. */
+	version: NamedAPIResource;
+	/** How often the item is held. */
+	rarity: number;
+}
+export interface PokemonMove {
+	/** The move the Pokémon can learn. */
+	move: NamedAPIResource;
+	/** The details of the version in which the Pokémon can learn the move. */
+	version_group_details: Array<PokemonMoveVersion>;
+}
+
+export interface PokemonMoveVersion {
+	/** The method by which the move is learned. */
+	move_learn_method: NamedAPIResource;
+	/** The version group in which the move is learned. */
+	version_group: NamedAPIResource;
+	/** The minimum level to learn the move. */
+	level_learned_at: number;
+}
+
+export interface PokemonStat {
+	/** The stat the Pokémon has. */
+	stat: NamedAPIResource;
+	/** The effort points (EV) the Pokémon has in the stat. */
+	effort: number;
+	/** The base value of the stat. */
+	base_stat: number;
+}
+
+export interface PokemonSprites {
+	front_default: string;
+	front_shiny: string;
+	front_female: string;
+	front_shiny_female: string;
+	back_default: string;
+	back_shiny: string;
+	back_female: string;
+	back_shiny_female: string;
+}
+
+/** Pokemon Location Areas */
+export interface LocationAreaEncounter {
+	/** The location area the referenced Pokémon can be encountered in. */
+	location_area: NamedAPIResource;
+	/** A list of versions and encounters with the referenced Pokémon that might happen. */
+	version_details: Array<VersionEncounterDetail>;
+}
+
+/** Pokemon Colors */
+export interface PokemonColor {
+	/** The identifier for this resource. */
+	id: number;
+	/** The name for this resource. */
+	name: string;
+	/** The name of this resource listed in different languages. */
+	names: Array<Name>;
+	/** A list of the Pokémon species that have this color. */
+	pokemon_species: Array<NamedAPIResource>;
+}
+
+/** Pokemon Forms */
+export interface PokemonForm {
+	/** The identifier for this resource. */
+	id: number;
+	/** The name for this resource. */
+	name: string;
+	/** The order in which forms should be sorted within all forms. Multiple forms may have equal order, in which case they should fall back on sorting by name. */
+	order: number;
+	/** The order in which forms should be sorted within a species' forms. */
+	form_order: number;
+	/** True for exactly one form used as the default for each Pokémon. */
+	is_default: boolean;
+	/** Whether or not this form can only happen during battle.*/
+	is_battle_only: boolean;
+	/** Whether or not this form requires mega evolution.*/
+	is_mega: boolean;
+	/** The name of this form. */
+	form_name: string;
+	/** The Pokémon that can take on this form. */
+	pokemon: NamedAPIResource;
+	/** A list of details showing types this Pokémon form has. */
+	types: Array<PokemonFormType>;
+	/** A set of sprites used to depict this Pokémon form in the game. */
+	sprites: PokemonFormSprites;
+	/** The version group this Pokémon form was introduced in. */
+	version_group: NamedAPIResource;
+	/** The form specific full name of this Pokémon form, or empty if the form does not have a specific name. */
+	names: Array<Name>;
+	/** The form specific form name of this Pokémon form, or empty if the form does not have a specific name. */
+	form_names: Array<Name>;
+}
+
+export interface PokemonFormSprites {
+	/** The default depiction of this Pokémon form from the front in battle. */
+	front_default: string;
+	/** The shiny depiction of this Pokémon form from the front in battle. */
+	front_shiny: string;
+	/** The default depiction of this Pokémon form from the back in battle. */
+	back_default: string;
+	/** The shiny depiction of this Pokémon form from the back in battle. */
+	back_shiny: string;
+}
+
+/** Pokemon Habitats */
+export interface PokemonHabitat {
+	/** The identifier for this resource. */
+	id: number;
+	/** The name for this resource. */
+	name: string;
+	/** The name of this resource listed in different languages. */
+	names: Array<Name>;
+	/** A list of the Pokémon species that can be found in this habitat. */
+	pokemon_species: Array<NamedAPIResource>;
+}
+
+/** Pokemon Shapes */
+export interface PokemonShape {
+	/** The identifier for this resource. */
+	id: number;
+	/** The name for this resource. */
+	name: string;
+	/** The "scientific" name of this Pokémon shape listed in different languages. */
+	awesome_names: Array<AwesomeName>;
+	/** The name of this resource listed in different languages. */
+	names: Array<Name>;
+	/** A list of the Pokémon species that have this shape. */
+	pokemon_species: Array<NamedAPIResource>;
+}
+
+export interface AwesomeName {
+	/** The localized "scientific" name for an API resource in a specific language. */
+	awesome_name: string;
+	/** The language this "scientific" name is in. */
+	language: NamedAPIResource;
+}
+
+/** Pokemon Species */
+export interface PokemonSpecies {
+	/** The identifier for this resource. */
+	id: number;
+	/** The name for this resource. */
+	name: string;
+	/** The order in which species should be sorted. Based on National Dex order, except families are grouped together and sorted by stage. */
+	order: number;
+	/** The chance of this Pokémon being female, in eighths; or -1 for genderless. */
+	gender_rate: number;
+	/** The base capture rate; up to 255. The higher the number, the easier the catch. */
+	capture_rate: number;
+	/** The happiness when caught by a normal Pokéball; up to 255. The higher the number, the happier the Pokémon. */
+	base_happiness: number;
+	/** Whether or not this is a baby Pokémon. */
+	is_baby: boolean;
+	/** Whether or not this is a legendary Pokémon. */
+	is_legendary: boolean;
+	/** Whether or not this is a mythical Pokémon. */
+	is_mythical: boolean;
+	/** Initial hatch counter: one must walk 255 × (hatch_counter + 1) steps before this Pokémon's egg hatches, unless utilizing bonuses like Flame Body's. */
+	hatch_counter: number;
+	/** Whether or not this Pokémon has visual gender differences. */
+	has_gender_differences: boolean;
+	/** Whether or not this Pokémon has multiple forms and can switch between them. */
+	forms_switchable: boolean;
+	/** The rate at which this Pokémon species gains levels. */
+	growth_rate: NamedAPIResource;
+	/** A list of Pokedexes and the indexes reserved within them for this Pokémon species. */
+	pokedex_numbers: Array<PokemonSpeciesDexEntry>;
+	/** A list of egg groups this Pokémon species is a member of. */
+	egg_groups: Array<NamedAPIResource>;
+	/** The color of this Pokémon for Pokédex search. */
+	color: NamedAPIResource;
+	/** The shape of this Pokémon for Pokédex search. */
+	shape: NamedAPIResource;
+	/** The Pokémon species that evolves into this Pokemon_species. */
+	evolves_from_species: NamedAPIResource;
+	/** The evolution chain this Pokémon species is a member of. */
+	evolution_chain: APIResource;
+	/** The habitat this Pokémon species can be encountered in. */
+	habitat: NamedAPIResource;
+	/** The generation this Pokémon species was introduced in. */
+	generation: NamedAPIResource;
+	/** The name of this resource listed in different languages. */
+	names: Array<Name>;
+	/** A list of encounters that can be had with this Pokémon species in pal park. */
+	pal_park_encounters: Array<PalParkEncounterArea>;
+	/** A list of flavor text entries for this Pokémon species. */
+	flavor_text_entries: Array<FlavorText>;
+	/** Descriptions of different forms Pokémon take on within the Pokémon species. */
+	form_descriptions: Array<Description>;
+	/** The genus of this Pokémon species listed in multiple languages. */
+	genera: Array<Genus>;
+	/** A list of the Pokémon that exist within this Pokémon species. */
+	varieties: Array<PokemonSpeciesVariety>;
+}
+
+export interface Genus {
+	/** The localized genus for the referenced Pokémon species */
+	genus: string;
+	/** The language this genus is in. */
+	language: NamedAPIResource;
+}
+
+export interface PokemonSpeciesDexEntry {
+	/** The index number within the Pokédex. */
+	entry_number: number;
+	/** The Pokédex the referenced Pokémon species can be found in. */
+	pokedex: NamedAPIResource;
+}
+
+export interface PalParkEncounterArea {
+	/** The base score given to the player when the referenced Pokémon is caught during a pal park run. */
+	base_score: number;
+	/** The base rate for encountering the referenced Pokémon in this pal park area. */
+	rate: number;
+	/** The pal park area where this encounter happens. */
+	area: NamedAPIResource;
+}
+
+export interface PokemonSpeciesVariety {
+	/** Whether this variety is the default variety. */
+	is_default: boolean;
+	/** The Pokémon variety. */
+	pokemon: NamedAPIResource;
+}
+
+/** Stats  */
